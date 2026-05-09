a2909bcb2b25e45d61086fd9980d29ab87d3e62d
Premium Technologies <37121311+premiumtechs@users.noreply.github.com>
2026-05-09 10:31:05 +0100

    Update binancepay.php

--- whmcs-binancepay/modules/gateways/callback/binancepay.php
+++ whmcs-binancepay/modules/gateways/callback/binancepay.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * Binance Pay Callback Handler
+ * Binance Pay WHMCS Callback
+ * 
+ * @package    Premium Technologies Private Limited
+ * @link       https://www.premiumtech.uk
  */
+
 require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
@@ -9,45 +13,23 @@ require_once __DIR__ . '/../../../includes/invoicefunctions.php';
 $gatewayModuleName = "binancepay";
 $gatewayParams = getGatewayVariables($gatewayModuleName);
 
-if (!$gatewayParams['type']) {
-    die("Module Not Activated");
-}
+if (!$gatewayParams['type']) die("Module Not Activated");
 
-$rawPayload = file_get_contents('php://input');
-$headers = array_change_key_case(getallheaders(), CASE_LOWER);
+$raw_payload = file_get_contents('php://input');
+$data = json_decode($raw_payload, true);
 
-$bSignature = isset($headers['binancepay-signature']) ? $headers['binancepay-signature'] : '';
-$bTimestamp = isset($headers['binancepay-timestamp']) ? $headers['binancepay-timestamp'] : '';
-$bNonce     = isset($headers['binancepay-nonce']) ? $headers['binancepay-nonce'] : '';
-
-// Verify Webhook Signature
-$payloadToVerify = $bTimestamp . "\n" . $bNonce . "\n" . $rawPayload . "\n";
-$expectedSignature = strtoupper(hash_hmac('sha512', $payloadToVerify, $gatewayParams['secretKey']));
-
-if ($bSignature !== $expectedSignature) {
-    logTransaction($gatewayParams['name'], $rawPayload, "Invalid Webhook Signature");
-    exit("Signature mismatch");
-}
-
-$data = json_decode($rawPayload, true);
-if ($data['bizType'] == 'PAY' && $data['bizStatus'] == 'PAY_SUCCESS') {
-    $bizData = json_decode($data['bizData'], true);
-    
-    // Extract Invoice ID
-    $tradeNo = $bizData['merchantTradeNo'];
-    $invoiceId = preg_replace("/[^0-9]/", "", explode('T', $tradeNo)[0]);
+if ($data['bizStatus'] === 'PAY_SUCCESS') {
+    $invoiceId = str_replace('Invoice #', '', $data['data']['goods']['goodsName']);
+    $transactionId = $data['data']['transactionId'];
+    $paymentAmount = $data['data']['orderAmount'];
 
     $invoiceId = checkCbInvoiceID($invoiceId, $gatewayParams['name']);
-    checkCbTransID($bizData['tradeGuid']); 
-
-    addInvoicePayment(
-        $invoiceId,
-        $bizData['tradeGuid'],
-        $bizData['orderAmount'],
-        0,
-        $gatewayModuleName
-    );
-
-    logTransaction($gatewayParams['name'], $data, "Payment Successful");
-    echo json_encode(array("returnCode" => "SUCCESS", "returnMessage" => null));
+    checkCbTransID($transactionId);
+    addInvoicePayment($invoiceId, $transactionId, $paymentAmount, 0, $gatewayModuleName);
+    
+    logTransaction($gatewayParams['name'], $raw_payload, "Success");
+    echo "SUCCESS";
+} else {
+    logTransaction($gatewayParams['name'], $raw_payload, "Failed or Pending");
+    echo "FAIL";
 }
